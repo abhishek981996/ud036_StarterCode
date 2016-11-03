ae2801258af1fe730894936b8784e68799cde29f
Abhishek tiwari <abhishektiwari981996@gmail.com>
2016-11-03 17:57:28 +0530

    Update file.py
    more description and docstring done ..

--- file.py
+++ file.py
@@ -1,13 +1,35 @@
-import fresh_tomatoes
+import fresh_tomatoes 
+''' using various class from fresh_tomatoes module(file) which can be seen in the list of files'''
 import media
+''' importing module name media which is used to create space or instance variables for objects like bb_ki_vines etc'''
 
-bb_ki_vines = media.Movie("bb ki vines","happy naukar episode","http://s1.dmcdn.net/Nq7Px/1280x720-zAK.jpg","https://www.youtube.com/watch?v=w0w7PrZS40w")
 
-bb_ki_vine = media.Movie("bb ki vines","fameer fudde episode","https://i.ytimg.com/vi/j1WL3Ysi9jo/maxresdefault.jpg","https://www.youtube.com/watch?v=1RN1e49h9NE")
+''' various arguments are passed in below objects to form a movie display details 
+ arguments like title, description, image, link is passed'''
 
-toy_story = media.Movie("toystory","toy story movie trailer","http://harlemtoys.com/wp-content/uploads/2016/03/toy-story-2-characters.jpg","https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+bb_ki_vines = media.Movie("bb ki vines",
+                          "happy naukar episode",
+                          "http://s1.dmcdn.net/Nq7Px/1280x720-zAK.jpg",
+                          "https://www.youtube.com/watch?v=w0w7PrZS40w")
 
-shivay = media.Movie("shivay", "shivay movie trailer","https://upload.wikimedia.org/wikipedia/en/a/a1/Ajay_Devgan's_Shivaay_poster.jpg","https://www.youtube.com/watch?v=poLjq0u4_5A")
+''' Instance or object formation'''
+bb_ki_vine  = media.Movie("bb ki vines",
+                          "fameer fudde episode",
+                          "https://i.ytimg.com/vi/j1WL3Ysi9jo/maxresdefault.jpg",
+                          "https://www.youtube.com/watch?v=1RN1e49h9NE")
 
-movie = [bb_ki_vines, bb_ki_vine, toy_story, shivay]
+toy_story   = media.Movie("toystory",
+                          "toy story movie trailer",
+                          "http://harlemtoys.com/wp-content/uploads/2016/03/toy-story-2-characters.jpg",
+                          "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+shivay  = media.Movie("shivay",
+                      "shivay movie trailer",
+                      "https://upload.wikimedia.org/wikipedia/en/a/a1/Ajay_Devgan's_Shivaay_poster.jpg",
+                      "https://www.youtube.com/watch?v=poLjq0u4_5A")
+'''passing the object from instance shivay to class Movie of module media'''
+
+movie   = [bb_ki_vines, bb_ki_vine, toy_story, shivay]
+'''creating a list of movies so that it can be passed in open_movies_page of fresh_tomatoes module inorder to display the html file'''
 fresh_tomatoes.open_movies_page(movie)
+''' passing movie list inorder to generate html file'''
